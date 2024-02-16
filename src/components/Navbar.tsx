caa4220a09ad7c1acec74211b1cb42a51d9a1b15
Nabin Hamal <nabinhamal2@gmail.com>
2024-02-16 09:46:01 +0545

    mobile navadded

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,20 @@
 import Link from 'next/link'
 import MaxWidthWrapper from './MaxWidthWrapper'
 import { Icons } from './Icons'
-import NavItems from './Navitems'
+
 import { buttonVariants } from './ui/button'
 import Cart from './Cart'
 import { getServerSideUser } from '@/lib/payload-utils'
-import {cookies} from "next/headers"
-import UserAccountNav from "./UserAccountNav"
+import { cookies } from 'next/headers'
+import UserAccountNav from './UserAccountNav'
+
+import NavItems from './Navitems'
+import MobileNav from './MobileNav'
+
 
 const Navbar = async () => {
   const nextCookies = cookies()
- const {user} = await getServerSideUser(nextCookies)
+  const { user } = await getServerSideUser(nextCookies)
 
   return (
     <div className='bg-white sticky z-50 top-0 inset-x-0 h-16'>
@@ -18,7 +22,7 @@ const Navbar = async () => {
         <MaxWidthWrapper>
           <div className='border-b border-gray-200'>
             <div className='flex h-16 items-center'>
-             {/**mobile nav */}
+              <MobileNav />
 
               <div className='ml-4 flex lg:ml-0'>
                 <Link href='/'>
@@ -27,31 +31,61 @@ const Navbar = async () => {
               </div>
 
               <div className='hidden z-50 lg:ml-8 lg:block lg:self-stretch'>
-                <NavItems/>
+                <NavItems />
               </div>
-            <div className="ml-auto flex items-center">
-              <div className="hidden lg:flex lg:flex-1 lg:items-center lg:justify-end  lg:space-x-6 ">
-{user ? null : (<Link href='/sign-in' className={buttonVariants({
-  variant: 'ghost',
-})}>
-  Sign In</Link>)}
 
-  {user ? null : (<span className='h-6 w-px bg-gray-200' aria-hidden='true'/>)}
-  {user ? <UserAccountNav user={user}/> :  (<Link href='/sign-up' className={buttonVariants({
-  variant: 'ghost',
-})}>
-  Create Account</Link>)}
+              <div className='ml-auto flex items-center'>
+                <div className='hidden lg:flex lg:flex-1 lg:items-center lg:justify-end lg:space-x-6'>
+                  {user ? null : (
+                    <Link
+                      href='/sign-in'
+                      className={buttonVariants({
+                        variant: 'ghost',
+                      })}>
+                      Sign in
+                    </Link>
+                  )}
 
-  {user ? (<span className='h-6 w-px bg-gray-200' aria-hidden='true'/>) : null}
-  {user? null :( <div className='flex lg:ml-6'>
-    <span className='h-6 w-px bg-gray-200' aria-hidden='true'/>
-  </div> )}
-  <div className="ml-4 flow-root lg:ml-6">
-    <Cart/>
-  </div>
+                  {user ? null : (
+                    <span
+                      className='h-6 w-px bg-gray-200'
+                      aria-hidden='true'
+                    />
+                  )}
+
+                  {user ? (
+                    <UserAccountNav user={user} />
+                  ) : (
+                    <Link
+                      href='/sign-up'
+                      className={buttonVariants({
+                        variant: 'ghost',
+                      })}>
+                      Create account
+                    </Link>
+                  )}
+
+                  {user ? (
+                    <span
+                      className='h-6 w-px bg-gray-200'
+                      aria-hidden='true'
+                    />
+                  ) : null}
+
+                  {user ? null : (
+                    <div className='flex lg:ml-6'>
+                      <span
+                        className='h-6 w-px bg-gray-200'
+                        aria-hidden='true'
+                      />
+                    </div>
+                  )}
+
+                  <div className='ml-4 flow-root lg:ml-6'>
+                    <Cart />
+                  </div>
+                </div>
               </div>
-            </div>
-              
             </div>
           </div>
         </MaxWidthWrapper>
